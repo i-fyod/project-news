48b01dd932079954481909ebb79e45fbab436a8e
i-fyod <i.fyod@ya.ru>
2025-02-19 10:59:35 +0300

    Added routing

--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -3,6 +3,7 @@ export { ThemeProvider } from "./context/theme/ThemeProvider";
 export { useColor } from "./hooks/useColor";
 export { useResolution } from "./hooks/useResolution";
 export { useDebounce } from "./hooks/useDebounce";
+export { useUrlParams } from "./hooks/useUrlParams";
 
 export { ThemeContext } from "./context/theme/ThemeContext";
 export type { IThemeContext } from "./context/theme/ThemeContext";
